c1bc3f44d79c2fff3bb20255e384ccd00b68d707
Occult09 <maheermusfiq221@gmail.com>
2026-03-03 14:51:59 +0600

    Loaded job data for hoome page

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,12 @@
+const CategoryList = () => {
+    return (
+        <div>
+            <div className="text-center">
+                <h2 className="text-5xl font-extrabold">Job Category List</h2>
+                <p className="font-medium text-gray-400">Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+        </div>
+    );
+};
+
+export default CategoryList;
