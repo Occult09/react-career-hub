4af35d1392c96656a8f26eadc1c8db84172f8202
Occult09 <maheermusfiq221@gmail.com>
2026-03-06 17:26:35 +0600

    loaded featured jobs data in cards and added functionality to see all jobs button

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -4,6 +4,7 @@ import Job from "../Job/Job";
 const FeaturedJobs = () => {
 
     const [jobs, setJobs] = useState([])
+    const [displayedJobs, setDisplayedJobs] = useState(4)
 
     useEffect(() => {
         fetch('jobs.json')
@@ -17,10 +18,15 @@ const FeaturedJobs = () => {
                 <h2 className="text-5xl font-extrabold">Featured Jobs</h2>
                 <p className="font-medium text-gray-400">Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
-            <div>
+            <div className="grid grid-cols-2">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, displayedJobs).map(job => <Job key={job.id} job={job}></Job>)
                 }
+            </div>
+            <div className={displayedJobs === jobs.length ? 'hidden' : ''}>
+                <div className="my-5 text-center">
+                    <button onClick={() => setDisplayedJobs(jobs.length)} className="btn btn-primary">See All Jobs</button>
+                </div>
             </div>
         </div>
     );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,10 +1,29 @@
 const Job = ({ job }) => {
 
-    const {logo} = job
+    const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary, job_description, } = job
 
     return (
-        <div>
-            <img src={logo} />
+        <div className="card bg-base-100 shadow-sm rounded">
+            <div className="card-body">
+                <figure className="w-30">
+                    <img
+                        src={logo}
+                        alt="Company Logo" />
+                </figure>
+                <h2 className="card-title text-2xl font-extrabold text-[#A6A6A6]">{job_title}</h2>
+                <p className="text-[20px] font-semibold text-[#9DA1AA]">{company_name}</p>
+                <div>
+                    <button className="btn text-[#7E90FE] font-extrabold border border-[#7E90FE] rounded px-5 py-2 mr-4">{remote_or_onsite}</button>
+                    <button className="btn text-[#7E90FE] font-extrabold border border-[#7E90FE] rounded px-5 py-2">{job_type}</button>
+                </div>
+                <div className="flex">
+                    <p> {location}</p>
+                    <p>{salary}</p>
+                </div>
+                <div className="card-actions">
+                    <button className="btn btn-primary">View Details</button>
+                </div>
+            </div>
         </div>
     );
 };
